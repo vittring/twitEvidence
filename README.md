436d074f0932fe9436201a7ae127e2988a279906
0xArchitect <vittring@riseup.net>
2021-06-30 01:08:39 -0400

    upload twitEvidence.py

---
+++ README.md
@@ -0,0 +1,8 @@
+I haven't coded a lot of python scripts but I decided to do some scraping.
+This program uses HTTrack and youtube-dl to automagically scrape URLs using 
+a file with links to crawl. HTTrack is available here: https://www.httrack.com/
+
+I originally used this program to capture videos and photos from a user's 
+timeline but this can be altered in any way you see fit. The HTTrack program
+is also free (GPL) Libre/free software. I suppose you could proxy it using Tor
+or your own proxies, or just feed it a file of proxies and import threading.
---
+++ twitEvidence.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 
+# This file is part of twitEvidence.
+# Only change this if you know what exactly it does.
+# It is obviously traceable without torifying/tsocks.
+
+import subprocess
+import shlex
+import os
+
+CMD_HTTrack = "httrack {URLS} +* -r1 -O twitter/httrack"
+CMD_YDL = "youtube-dl -i {URLS} -o ./twitter/videos/%(title)s.%(ext)s"
+
+# create directories
+os.makedirs('./twitter/videos', exist_ok=True)
+os.makedirs('./twitter/httrack', exist_ok=True)
+
+# place all links to scrape in file "links"
+with open('links', 'r') as f:
+
+    # concatenate all urls together
+    line = f.readline().rstrip('\n')
+    urls = ''
+    while line:
+        urls += line + ' '
+        line = f.readline().rstrip('\n')
+
+    # start scraping using HTTrack
+    run = subprocess.Popen(shlex.split(CMD_HTTrack.format(
+            URLS=urls
+    run.wait()
+
+    # also scrape using youtube-dl (videos only)
+    run = subprocess.Popen(shlex.split(CMD_YDL.format(
+            URLS=urls
+    run.wait()
